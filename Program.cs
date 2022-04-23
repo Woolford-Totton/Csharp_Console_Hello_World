f0a06c91edd94724501e7263f55cea749e7e6e83
BenWoolford30 <benwoolford30@gmail.com>
2022-04-23 10:47:10 +0100

    Hello world project
    putting project onto GitHub along with presentation

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿#region ""
+using System;
+
+namespace LS_1_Hello_World
+{
+    class Program
+    {
+        #endregion
+    /*
+        Welcome to your first programme- to start run the program by pressing the green play button OR by pressing F5.
+        
+        The very first thing you will see is a window appears with text saying Hello World!
+        
+        Once you see it close it and try changing the text to output something else.
+    */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+            
+
+        }
+#region "" 
+    }
+}
+#endregion
